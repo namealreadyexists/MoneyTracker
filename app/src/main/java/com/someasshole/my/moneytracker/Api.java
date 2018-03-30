e62a32152d990bcbaabcd41b3a9e6f56d08bd8e1
Helen <1_nae@mail.ru>
2018-03-30 20:36:08 +0700

    exercise_09

--- app/src/main/java/com/someasshole/my/moneytracker/Api.java
+++ app/src/main/java/com/someasshole/my/moneytracker/Api.java
@@ -1,8 +1,8 @@
 package com.someasshole.my.moneytracker;
 
+import java.util.List;
+
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -11,18 +11,12 @@ public interface Api {
     @GET("auth")
     Call<AuthResult> auth(@Query("social_user_id") String userID);
 
-    @GET("items/add")
-    Call<AddItemResult> addItem(@Query("price") String price, @Query("name") String name,@Query("type") String type);
+    @POST("items/add")
+    Call<AddItemResult> addItem(@Query("price") int price, @Query("name") String name,@Query("type") String type);
+
+    @GET("items")
+    Call<List<Record>> getItems(@Query("type") String type);
 
     @GET("balance")
     Call<BalanceResult> balance();
-
-    @GET("/items")
-    Call<ServerResponse> getItems(@Query("type") String type);
-
-    @POST("/items/add")
-    Call<ServerResponse> registerRecord(@Body RegisterRecordBody registerRecordBody);
-
-    @DELETE("/items")
-    Call<ServerResponse> deleteRecord(@Query("id") int id);
 }
